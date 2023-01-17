314ed4cc91e246e6aab93b1de6cda8a6287e4447
Wisnu Pri H <78286835+wisnuph@users.noreply.github.com>
2023-01-17 21:44:00 +0700

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -55,6 +55,24 @@ def main():
             ax2.axvline(x=idx_predict, linewidth=1, label='First break', color='red')
             ax2.legend()
             st.pyplot(fig1)
+            
+            st.markdown('*Note: The arrival of the wave-P is determined based on the highest value of the machine learning output*')
+            
+        if st.button('Predict with filterisasi Trace'):
+            trace_n = preprocess(trace_n, 1, 3)
+            df = seismogram.make_parameters(trace_n, 2, 30, 40)
+            df = np.resize(df, (df.shape[0], 1, df.shape[1]))
+            predict = savedModel.predict(df)
+            predict = np.resize(predict, (predict.shape[1], predict.shape[0]))
+            idx_predict = predict[0].argmax()
+
+            fig2, (ax1, ax2) = plt.subplots(2, 1, figsize=(15,10))
+            ax1.plot(predict[0], label='Machine Learning Output', color='black')
+            ax1.legend()
+            ax2.plot(trace_n[0].data, label='Trace Example', color='black')
+            ax2.axvline(x=idx_predict, linewidth=1, label='First break', color='red')
+            ax2.legend()
+            st.pyplot(fig2)
 
             st.markdown('*Note: The arrival of the wave-P is determined based on the highest value of the machine learning output*')
 
